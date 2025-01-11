5137d4bbaec3314ec55d8dd26fab09c245c3ec09
Asril13 <142967923+SriL13@users.noreply.github.com>
2025-01-11 08:44:06 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // Konfigurasi database
-$host = "localhost";      // Nama host (default: localhost)
-$user = "root";           // Username MySQL (default: root)
-$pass = "";               // Password MySQL (kosongkan jika default)
-$db   = "db_admin";       // Nama database
+$host = "103.171.161.6";      // Nama host (default: localhost)
+$user = "asril";           // Username MySQL (default: root)
+$pass = "Pass@1234";               // Password MySQL (kosongkan jika default)
+$db   = "ti_a_asril";       // Nama database
 
 // Membuat koneksi
 $conn = mysqli_connect($host, $user, $pass, $db);
